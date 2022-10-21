2133917ce4c108022779036fefa4bdc14ba21b9c
Daniel Zhang <daniel6628@gmail.com>
2022-10-20 22:02:44 -0400

    auth/login back to original

--- routes/auth.js
+++ routes/auth.js
@@ -7,7 +7,7 @@ const requireAuth = passport.authenticate('jwt', { session: false });
 const requireLogin = passport.authenticate('local', { session: false });
 
 module.exports = function (app) {
-  // app.post('/auth/login', requireLogin, Authentication.login);
-  app.post('/auth/login', Authentication.login);
+  app.post('/auth/login', requireLogin, Authentication.login);
+  // app.post('/auth/login', Authentication.login);
   app.post('/auth/register', Authentication.register);
 };
