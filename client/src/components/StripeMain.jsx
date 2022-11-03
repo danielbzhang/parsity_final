d0abd4614b1716d10d30202619549c3595698fdf
Daniel Zhang <daniel6628@gmail.com>
2022-11-03 10:44:29 -0400

    stripe public key in StripeMain.jsx

--- client/src/components/StripeMain.jsx
+++ client/src/components/StripeMain.jsx
@@ -3,7 +3,9 @@ import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import StripeForm from './StripeForm';
 
-const PUBLIC_KEY = `${process.env.REACT_APP_STRIPE_PUBLIC}`;
+// const PUBLIC_KEY = `${process.env.REACT_APP_STRIPE_PUBLIC}`;
+const PUBLIC_KEY =
+  'pk_test_51LrXV7GYfyzkTIYrVK2DafROIkKyH9org6xikSCYS1DvyNIKfL0rq1Oshv3WL8EsfAA51Njl8aQeSCMcv92Zjzy700tC84MKAX';
 const stripePromise = loadStripe(PUBLIC_KEY);
 
 const StripeMain = () => {
--- config/prod.js
+++ config/prod.js
@@ -2,5 +2,5 @@ module.exports = {
   MONGODB_URI: process.env.MONGODB_URI,
   TOKEN_SECRET: process.env.TOKEN_SECRET,
   STRIPE_SECRET: process.env.STRIPE_SECRET,
-  STRIPE_PUBLIC: process.env.STRIPE_PUBLIC,
+  REACT_APP_STRIPE_PUBLIC: process.env.STRIPE_PUBLIC,
 };
